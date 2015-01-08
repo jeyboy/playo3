66258a486154a79b5c48cec45ac4867b93d83a80
unknown <jeyboy1985@gmail.com>
2015-01-08 04:00:25 +0200

    totaly destroy dock

--- modules/controls/dockbar.cpp
+++ modules/controls/dockbar.cpp
@@ -3,6 +3,7 @@
 using namespace Playo3;
 
 DockBar::DockBar(const QString &title, QWidget *parent, Qt::WindowFlags flags) : QDockWidget(title, parent, flags) {
+    setAttribute(Qt::WA_DeleteOnClose);
     setObjectName("tool_" + title);
     setTitleBarWidget((titleWidget = new WindowTitle(this, 30, QMargins(0, 10, 0, 0), 0, false, false, false)));
     titleWidget -> addMaxiButton(this, SLOT(toggleFloating()));
