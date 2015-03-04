cf1a95f2d5f3c3ceac9da949ae28fadb22543747
unknown <jeyboy1985@gmail.com>
2015-03-04 20:56:33 +0200

    little refactor

--- modules/data_struct/hotkeys/hotkey_model.cpp
+++ modules/data_struct/hotkeys/hotkey_model.cpp
@@ -1,7 +1,7 @@
 #include "hotkey_model.h"
 #include <QDebug>
 
-HotkeyModel::HotkeyModel(QList<HotkeyModelItem *> * toplevelchilds, QObject *parent) : QAbstractItemModel(parent) {   
+HotkeyModel::HotkeyModel(QList<HotkeyModelItem * > * toplevelchilds, QObject * parent) : QAbstractItemModel(parent) {
     QVector<QVariant> rootData;
     rootData << "" << "";
     rootItem = new HotkeyModelItem(rootData);
@@ -16,11 +16,11 @@ int HotkeyModel::columnCount(const QModelIndex & /* parent */) const {
     return rootItem -> columnCount();
 }
 
-QVariant HotkeyModel::data(const QModelIndex &index, int role) const {
+QVariant HotkeyModel::data(const QModelIndex & index, int role) const {
     if (!index.isValid())
         return QVariant();
 
-    HotkeyModelItem *item;
+    HotkeyModelItem * item;
 
     if (role == Qt::UserRole) {
         item = getItem(index);
@@ -32,7 +32,7 @@ QVariant HotkeyModel::data(const QModelIndex &index, int role) const {
     return item -> data(index.column());
 }
 
-Qt::ItemFlags HotkeyModel::flags(const QModelIndex &index) const {
+Qt::ItemFlags HotkeyModel::flags(const QModelIndex & index) const {
     if (!index.isValid())
         return 0;
 
@@ -42,9 +42,9 @@ Qt::ItemFlags HotkeyModel::flags(const QModelIndex &index) const {
         return Qt::ItemIsEnabled | Qt::ItemIsSelectable;
 }
 
-HotkeyModelItem *HotkeyModel::getItem(const QModelIndex &index) const {
+HotkeyModelItem * HotkeyModel::getItem(const QModelIndex & index) const {
     if (index.isValid()) {
-        HotkeyModelItem *item = static_cast<HotkeyModelItem*>(index.internalPointer());
+        HotkeyModelItem * item = static_cast<HotkeyModelItem * >(index.internalPointer());
         if (item) return item;
     }
     return rootItem;
@@ -54,7 +54,7 @@ HotkeyModelItem * HotkeyModel::root() const {
     return rootItem;
 }
 
-QList<HotkeyModelItem *> HotkeyModel::toplevelItems() {
+QList<HotkeyModelItem * > HotkeyModel::toplevelItems() {
     return root() -> childList();
 }
 
@@ -65,88 +65,43 @@ QVariant HotkeyModel::headerData(int section, Qt::Orientation orientation, int r
     return QVariant();
 }
 
-QModelIndex HotkeyModel::index(int row, int column, const QModelIndex &parent) const {
+QModelIndex HotkeyModel::index(int row, int column, const QModelIndex & parent) const {
     if (parent.isValid() && parent.column() != 0)
         return QModelIndex();
 
-    HotkeyModelItem *parentItem = getItem(parent);
+    HotkeyModelItem * parentItem = getItem(parent);
 
-    HotkeyModelItem *childItem = parentItem->child(row);
+    HotkeyModelItem * childItem = parentItem -> child(row);
     if (childItem)
         return createIndex(row, column, childItem);
     else
         return QModelIndex();
 }
 
-//bool HotkeyModel::insertColumns(int position, int columns, const QModelIndex &parent) {
-//    bool success;
-
-//    beginInsertColumns(parent, position, position + columns - 1);
-//    success = rootItem->insertColumns(position, columns);
-//    endInsertColumns();
-
-//    return success;
-//}
-
-//bool HotkeyModel::insertRows(int position, int rows, const QModelIndex &parent) {
-//    HotkeyModelItem *parentItem = getItem(parent);
-//    bool success;
-
-//    beginInsertRows(parent, position, position + rows - 1);
-//    success = parentItem->insertChildren(position, rows, rootItem->columnCount());
-//    endInsertRows();
-
-//    return success;
-//}
-
-QModelIndex HotkeyModel::parent(const QModelIndex &index) const {
+QModelIndex HotkeyModel::parent(const QModelIndex & index) const {
     if (!index.isValid())
         return QModelIndex();
 
-    HotkeyModelItem *childItem = getItem(index);
-    HotkeyModelItem *parentItem = childItem->parent();
+    HotkeyModelItem * childItem = getItem(index);
+    HotkeyModelItem * parentItem = childItem -> parent();
 
     if (parentItem == rootItem || parentItem == 0)
         return QModelIndex();
 
-    return createIndex(parentItem->childNumber(), 0, parentItem);
+    return createIndex(parentItem -> childNumber(), 0, parentItem);
 }
 
-//bool HotkeyModel::removeColumns(int position, int columns, const QModelIndex &parent) {
-//    bool success;
-
-//    beginRemoveColumns(parent, position, position + columns - 1);
-//    success = rootItem->removeColumns(position, columns);
-//    endRemoveColumns();
-
-//    if (rootItem->columnCount() == 0)
-//        removeRows(0, rowCount());
-
-//    return success;
-//}
-
-//bool HotkeyModel::removeRows(int position, int rows, const QModelIndex &parent) {
-//    HotkeyModelItem *parentItem = getItem(parent);
-//    bool success = true;
-
-//    beginRemoveRows(parent, position, position + rows - 1);
-//    success = parentItem->removeChildren(position, rows);
-//    endRemoveRows();
-
-//    return success;
-//}
-
-int HotkeyModel::rowCount(const QModelIndex &parent) const {
-    HotkeyModelItem *parentItem = getItem(parent);
+int HotkeyModel::rowCount(const QModelIndex & parent) const {
+    HotkeyModelItem * parentItem = getItem(parent);
     return parentItem -> childCount();
 }
 
-bool HotkeyModel::setData(const QModelIndex &index, const QVariant &value, int role) {
+bool HotkeyModel::setData(const QModelIndex & index, const QVariant & value, int role) {
     if (role != Qt::EditRole)
         return false;
 
-    HotkeyModelItem *item = getItem(index);
-    bool result = item->setData(index.column(), value);
+    HotkeyModelItem * item = getItem(index);
+    bool result = item -> setData(index.column(), value);
 
     if (result)
         emit dataChanged(index, index);
@@ -155,11 +110,11 @@ bool HotkeyModel::setData(const QModelIndex &index, const QVariant &value, int r
 }
 
 bool HotkeyModel::setHeaderData(int section, Qt::Orientation orientation,
-                              const QVariant &value, int role) {
+                              const QVariant & value, int role) {
     if (role != Qt::EditRole || orientation != Qt::Horizontal)
         return false;
 
-    bool result = rootItem->setData(section, value);
+    bool result = rootItem -> setData(section, value);
 
     if (result)
         emit headerDataChanged(orientation, section, section);
--- modules/data_struct/hotkeys/hotkey_model.h
+++ modules/data_struct/hotkeys/hotkey_model.h
@@ -8,31 +8,28 @@ class HotkeyModel : public QAbstractItemModel {
     Q_OBJECT
 
 public:
-    HotkeyModel(QList<HotkeyModelItem *> * toplevelchilds = 0, QObject *parent = 0);
+    HotkeyModel(QList<HotkeyModelItem *> * toplevelchilds = 0, QObject * parent = 0);
     ~HotkeyModel();
 
-    QVariant data(const QModelIndex &index, int role) const;
-    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole);
+    QVariant data(const QModelIndex & index, int role) const;
+    bool setData(const QModelIndex & index, const QVariant &value, int role = Qt::EditRole);
     QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
-    bool setHeaderData(int section, Qt::Orientation orientation, const QVariant &value, int role = Qt::EditRole);
+    bool setHeaderData(int section, Qt::Orientation orientation, const QVariant & value, int role = Qt::EditRole);
 
-    Qt::ItemFlags flags(const QModelIndex &index) const;
-    QModelIndex parent(const QModelIndex &index) const;
-    QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const;
+    Qt::ItemFlags flags(const QModelIndex & index) const;
+    QModelIndex parent(const QModelIndex & index) const;
+    QModelIndex index(int row, int column, const QModelIndex & parent = QModelIndex()) const;
     QModelIndex index(HotkeyModelItem * item) const;
 
     int itemsCount() const;
 
-    int columnCount(const QModelIndex &parent = QModelIndex()) const;
+    int columnCount(const QModelIndex & parent = QModelIndex()) const;
 
-    int rowCount(const QModelIndex &parent = QModelIndex()) const;
-    void appendRow(HotkeyModelItem * item);
+    int rowCount(const QModelIndex & parent = QModelIndex()) const;
 
-    void refreshItem(HotkeyModelItem * item);
-
-    HotkeyModelItem *getItem(const QModelIndex &index) const;
+    HotkeyModelItem * getItem(const QModelIndex &index) const;
     HotkeyModelItem * root() const;
-    QList<HotkeyModelItem *> toplevelItems();
+    QList<HotkeyModelItem * > toplevelItems();
 
 protected:
     HotkeyModelItem * rootItem;
