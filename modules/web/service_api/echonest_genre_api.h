fefeafa0eb289433c9a3ac9b9eb2f605265a95ab
unknown <jeyboy1985@gmail.com>
2015-06-03 04:50:56 +0300

    temp

--- modules/web/service_api/echonest_genre_api.h
+++ modules/web/service_api/echonest_genre_api.h
@@ -16,9 +16,143 @@ class EchonestGenreApi : public IApi {
             return url;
         }
 
-        QJsonObject artistsByGenre(QString genre) {
+        //{
+        //    "response": {
+        //        "artists": [
+        //            {
+        //                "hotttnesss": 0.601159,
+        //                "id": "AR9PLH11187FB58A87",
+        //                "name": "Thelonious Monk"
+        //            },
+        //            {
+        //                "hotttnesss": 0.598763,
+        //                "id": "ARIOZCU1187FB3A3DC",
+        //                "name": "John Coltrane"
+        //            }
+        //        ],
+        //        "status": {
+        //            "code": 0,
+        //            "message": "Success",
+        //            "version": "4.2"
+        //        }
+        //    }
+        //}
 
+        inline QUrl genresUrl() {
+            QUrl url(baseUrl("genre/list"));
+            QUrlQuery query = buildDefaultParams();
+            url.setQuery(query);
+            return url;
         }
+
+        //{
+        //    "response": {
+        //        "genres": [
+        //            {
+        //                "name": "a cappella"
+        //            },
+        //            {
+        //                "name": "abstract hip hop"
+        //            }
+        //        ],
+        //        "start": 0,
+        //        "status": {
+        //            "code": 0,
+        //            "message": "Success",
+        //            "version": "4.2"
+        //        },
+        //        "total": 777
+        //    }
+        //}
+
+        inline QUrl genreInfoUrl(QString & genre) {
+            QUrl url(baseUrl("genre/profile"));
+            QUrlQuery query = buildDefaultParams();
+            setParam(query, "bucket", "description");
+            setParam(query, "name", genre);
+            url.setQuery(query);
+            return url;
+        }
+
+        //{
+        //    "response": {
+        //        "genres": [
+        //            {
+        //                "description": "Acid jazz, also called club jazz, is a style of jazz that takes cues from a number of genres, including funk,
+        //hip-hop, house, and soul.",
+        //                "name": "acid jazz"
+        //            }
+        //        ],
+        //        "status": {
+        //            "code": 0,
+        //            "message": "Success",
+        //            "version": "4.2"
+        //        }
+        //    }
+        //}
+
+        inline int serachGenreLimit() const { return 100; }
+        inline QUrl serachGenresUrl(QString & genre, int offset = 0) {
+            QUrl url(baseUrl("genre/search"));
+            QUrlQuery query = buildDefaultParams();
+            setLimit(query, serachGenreLimit(), offset);
+            setParam(query, "name", genre);
+            url.setQuery(query);
+            return url;
+        }
+
+        //{
+        //    "response": {
+        //        "genres": [
+        //            {
+        //                "name": "a cappella"
+        //            },
+        //            {
+        //                "name": "abstract hip hop"
+        //            }
+        //        ],
+        //        "start": 0,
+        //        "status": {
+        //            "code": 0,
+        //            "message": "Success",
+        //            "version": "4.2"
+        //        },
+        //        "total": 777
+        //    }
+        //}
+
+
+        inline int similarGenresLimit() const { return 100; }
+        inline QUrl similarGenresUrl(QString & genre, int offset = 0) {
+            QUrl url(baseUrl("genre/similar"));
+            QUrlQuery query = buildDefaultParams();
+            setLimit(query, serachGenreLimit(), offset);
+            setParam(query, "name", genre);
+            url.setQuery(query);
+            return url;
+        }
+
+        //{
+        //    "response": {
+        //        "genres": [
+        //            {
+        //                "name": "rock",
+        //                "similarity": 0.768293
+        //            },
+        //            {
+        //                "name": "hard rock",
+        //                "similarity": 0.539634
+        //            }
+        //        ],
+        //        "start": 0,
+        //        "status": {
+        //            "code": 0,
+        //            "message": "Success",
+        //            "version": "4.2"
+        //        },
+        //        "total": 19
+        //    }
+        //}
 };
 
 #endif // ECHONEST_GENRE_API
