c96797313f9b888f75d24715f6688966f74a63c0
unknown <jeyboy1985@gmail.com>
2015-09-05 11:16:34 +0300

    temp

--- dialogs/user_action_dialog.cpp
+++ dialogs/user_action_dialog.cpp
@@ -1,14 +1,42 @@
 #include "user_action_dialog.h"
 #include "ui_user_action_dialog.h"
 
-UserActionDialog::UserActionDialog(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::UserActionDialog)
-{
-    ui->setupUi(this);
+UserActionDialog::UserActionDialog(QWidget * parent) : QDialog(parent), ui(new Ui::UserActionDialog) {
+    ui -> setupUi(this);
+
+    new QGridLayout(this);
 }
 
-UserActionDialog::~UserActionDialog()
-{
+UserActionDialog::~UserActionDialog() {
     delete ui;
+}
+
+void UserActionDialog::buildLoginForm(const QString & login_name, const QString & password_name) {
+    QList<const FormInput &> inputs;
+    inputs << FormInput(text, QStringLiteral("login"), login_name);
+    inputs << FormInput(text, QStringLiteral("password"), password_name);
+}
+
+void UserActionDialog::buildCaptchaForm() {
+
+}
+
+void UserActionDialog::buildForm(QList<const FormInput &> inputs) {
+     delete layout();
+
+     QGridLayout * l = new QGridLayout(this);
+
+
+}
+
+void UserActionDialog::extendForm(QList<const FormInput &> inputs) {
+     delete layout();
+
+     QGridLayout * l = new QGridLayout(this);
+
+
+}
+
+QString UserActionDialog::getValue(const QString & name) {
+
 }
--- dialogs/user_action_dialog.h
+++ dialogs/user_action_dialog.h
@@ -1,22 +1,37 @@
 #ifndef USER_ACTION_DIALOG_H
 #define USER_ACTION_DIALOG_H
 
-#include <QDialog>
+#include <qdialog.h>
+#include <qgridlayout.h>
 
-namespace Ui {
-class UserActionDialog;
-}
+namespace Ui { class UserActionDialog; }
 
-class UserActionDialog : public QDialog
-{
+enum FormInputType { image, text };
+
+struct FormInput {
+    FormInput(FormInputType ftype, const QString & name, const QString & label, const QString & value) :
+        label(label), name(name), value(value), ftype(ftype) {}
+
+    QString label;
+    QString name;
+    QString value;
+    FormInputType ftype;
+};
+
+class UserActionDialog : public QDialog {
     Q_OBJECT
-
 public:
-    explicit UserActionDialog(QWidget *parent = 0);
+    explicit UserActionDialog(QWidget * parent = 0);
     ~UserActionDialog();
 
+    void buildLoginForm(const QString & login_name = QStringLiteral("Login"), const QString & password_name = QStringLiteral("Password"));
+    void buildCaptchaForm();
+    void buildForm(QList<const FormInput &> inputs);
+    void extendForm(QList<const FormInput &> inputs);
+
+    QString getValue(const QString & name);
 private:
-    Ui::UserActionDialog *ui;
+    Ui::UserActionDialog * ui;
 };
 
 #endif // USER_ACTION_DIALOG_H
