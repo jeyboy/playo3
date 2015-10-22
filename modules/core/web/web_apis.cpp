130d5f0d1de85136796f42491bded08931f1ff4e
JB <jeyboy@bigmir.net>
2015-10-22 12:42:38 +0300

    fixes

--- modules/core/web/web_apis.cpp
+++ modules/core/web/web_apis.cpp
@@ -13,15 +13,15 @@ namespace Core {
             sites.insert(Fourshared::Api::instance() -> siteType(), Fourshared::Api::instance(obj.value(SETTINGS_FOURSHARED_SET_KEY).toObject()));
             sites.insert(Od::Api::instance() -> siteType(), Od::Api::instance(parent, obj.value(SETTINGS_OD_SET_KEY).toObject()));
 
-            sites.insert(MyzukaAlbum::instance() -> siteType(), MyzukaAlbum::instance());
-            sites.insert(Zaycev::instance() -> siteType(), Zaycev::instance());
-            sites.insert(Mp3Base::instance() -> siteType(), Mp3Base::instance());
-            sites.insert(PromoDj::instance() -> siteType(), PromoDj::instance());
-            sites.insert(Mp3cc::instance() -> siteType(), Mp3cc::instance());
-            sites.insert(Mp3pm::instance() -> siteType(), Mp3pm::instance());
-            sites.insert(Shmidt::instance() -> siteType(), Shmidt::instance());
+            sites.insert(MyzukaAlbum::obj().siteType(), &MyzukaAlbum::obj());
+            sites.insert(Zaycev::obj().siteType(), &Zaycev::obj());
+            sites.insert(Mp3Base::obj().siteType(), &Mp3Base::obj());
+            sites.insert(PromoDj::obj().siteType(), &PromoDj::obj());
+            sites.insert(Mp3cc::obj().siteType(), &Mp3cc::obj());
+            sites.insert(Mp3pm::obj().siteType(), &Mp3pm::obj());
+            sites.insert(Shmidt::obj().siteType(), &Shmidt::obj());
             sites.insert(Jetune::obj().siteType(), &Jetune::obj());
-            sites.insert(MusicShara::instance() -> siteType(), MusicShara::instance());
+            sites.insert(MusicShara::obj().siteType(), &MusicShara::obj());
         }
     }
 }
