43a0c2ae8b8ff5749b2290f2932122d88c57849f
JB <jeyboy1985@gmail.com>
2016-03-22 22:03:32 +0200

    update default lib save delay]

--- settings/library_settings.cpp
+++ settings/library_settings.cpp
@@ -3,7 +3,7 @@
 void LibrarySettings::fromJson(const QJsonObject & settings) {
     _remoteItemsProcDelay = settings.value(SETTINGS_REMOTE_LIB_PROC_DELAY_KEY).toInt(1000); // in msec
 
-    _saveLibDelay = settings.value(SETTINGS_SAVE_LIB_DELAY_KEY).toInt(10000); // in msec
+    _saveLibDelay = settings.value(SETTINGS_SAVE_LIB_DELAY_KEY).toInt(600000); // in msec
 
     _initiate_on_play = settings.value(SETTINGS_INITIATE_ON_PLAYING_KEY).toBool(true);
     _showInfo = settings.value(SETTINGS_SHOW_INFO_KEY).toBool(true);
