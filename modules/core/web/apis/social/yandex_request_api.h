3190c102d36018d397208acfe4075d5dbd3c89f2
JB <jeyboy1985@gmail.com>
2016-05-22 20:30:17 +0300

    temp

--- modules/core/web/apis/social/yandex_request_api.h
+++ modules/core/web/apis/social/yandex_request_api.h
@@ -5,7 +5,6 @@
 #include "modules/core/web/interfaces/auth/teu_auth.h"
 #include "modules/core/web/interfaces/iapi.h"
 
-//#define YANDEX_OFFSET_LIMIT 2000
 #define YANDEX_SEARCH_LIMIT 200
 
 namespace Core {
@@ -35,18 +34,18 @@ namespace Core {
                 //experiments={"feedTracksByGenre":"default","loserFeedGenreTop":"chart-second","feedBrick":"default","rotorLoserHistoryToIchwill":"default","rotorLessDashboards":"default","rotorPersonalStationOn":"on","rotorLoserAllSessions":"default","rotorNewGenres":"default","userRadio":"ichwill","playlistOfTheDayAds":"ads","feedNewCandidates":"default","feedNewAlbumsOfLikedLabel":"default","rotorGreatestHits":"default","rotorOtherUserRadio":"default","feedDoublingBlocks":"doubling","rotorTrueDeboyanizator":"on","feedMissedTracksByArtist":"default","feedLogNotShownBlocks":"default","feedMixed":"default","useNewItemStorage":"default","rbt":"on","feedRecentTrackLikeTracks":"default","userFeed":"feed22","rotorIchwillRadio":"radio5_new__penalize_like_ratio__use_new_personal_station","webAds":"no-ads","showRadio":"default","feedRareArtist":"default","feedDither":"default","rotorStations":"simple_dashboard_formula_expected-time_track_finished_1_spav__1","rotorAds":"no-ads","feedWizardAsFilter":"default","feedDropTimeoutsAtMidnight":"on","radioTab":"place","artistWikiFacts":"on","feedArtistByFriends":"default","feedPlaylistOfTheDayFormula":"feed_potd_5_anti_boyan_100","personalTops":"enabled","feedAfishaNewApi":"on","paywall":"default","feedLongMemory":"default","rotorSafeTopTracksCount":"twelve","rotorPizza":"fallback","rotorPersonalStationOnMobile":"on","rotorPersonalProgressComposite":"default","feedTriggers":"default","feedCounters":"default","rotorCandidatesSampling":"default","feedUseNewWizard":"on","similarities":"default","genreRadio":"rotator","recommendedArtists":"ichwill_similar_artists","recommendedTracks":"recommended_tracks_by_artist_from_history","recommendedAlbumsOfFavoriteGenre":"recent","recommendedSimilarArtists":"default","recommendedArtistsWithArtistsFromHistory":"force_recent","adv":"d","loserArtistsWithArtists":"off"}
 
                 // array of {id, name, cover, genres, ratings}
-                inline QUrl artistInfoUrl(const QString & artistId) { return url_api_v1 + (QStringLiteral("artist.jsx?artist=%1").arg(artistId)); }
+                inline QString artistInfoUrl(const QString & artistId) { return url_api_v1 + (QStringLiteral("artist.jsx?artist=%1").arg(artistId)); }
                 //artist // similar // albums // alsoAlbums // tracks // radio // alsoAlbumIds // albumIds // concerts
-                inline QUrl artistTracksUrl(const QString & artistId) { return url_api_v1 + QStringLiteral("artist.jsx?artist=%1&what=tracks&sort=&dir=").arg(artistId); }
-                inline QUrl artistAlbumsUrl(const QString & artistId) { return url_api_v1 + QStringLiteral("artist.jsx?artist=%1&what=albums&sort=&dir=").arg(artistId); }
-                inline QUrl artistSimilarUrl(const QString & artistId) { return url_api_v1 + QStringLiteral("artist.jsx?artist=%1&what=similar&sort=&dir=").arg(artistId); }
+                inline QString artistTracksUrl(const QString & artistId) { return url_api_v1 + QStringLiteral("artist.jsx?artist=%1&what=tracks&sort=&dir=").arg(artistId); }
+                inline QString artistAlbumsUrl(const QString & artistId) { return url_api_v1 + QStringLiteral("artist.jsx?artist=%1&what=albums&sort=&dir=").arg(artistId); }
+                inline QString artistSimilarUrl(const QString & artistId) { return url_api_v1 + QStringLiteral("artist.jsx?artist=%1&what=similar&sort=&dir=").arg(artistId); }
 
                 // id, title, year, cover, coverUri, genre, artists, labels: [{id, name}], volumes: [[{id, realId, title, durationMs, fileSize, storageDir, available}, ..]]
-                inline QUrl albumInfoUrl(const QString & albumId) { return url_api_v1 + QStringLiteral("albums.jsx?albumIds=%1").arg(albumId); }
+                inline QString albumInfoUrl(const QString & albumId) { return url_api_v1 + QStringLiteral("albums.jsx?albumIds=%1").arg(albumId); }
 
                 // __t=1451812081991
                 // array of { id, realId, title, fileSize, storageDir, durationMs, embedPlayback, artists: {id, name, decomposed: ["feat or vs", {id, name}, ..]}}
-                inline QUrl traksUrl(const QStringList & trackIds) {
+                inline QString traksUrl(const QStringList & trackIds) {
                     QString res = url_api_v2 % QStringLiteral("tracks?");
 
                     for(QStringList::ConstIterator trackId = trackIds.constBegin(); trackId != trackIds.constEnd(); trackId++)
@@ -57,38 +56,49 @@ namespace Core {
 
                 //__t=1451823793515
                 // codec, bitrate, src, gain
-                inline QUrl trackDownloadingUrl(const QString & trackId) { return url_api_v2 + QStringLiteral("track/%1/download").arg(trackId); }
+                inline QString trackDownloadingUrl(const QString & trackId) { return url_api_v2 + QStringLiteral("track/%1/download").arg(trackId); }
 
 
                 //filter = albums: genre, pager: {total, page, perPage}, albums,
                 //filter = : genre, tracks, artists, albums, pics
-                inline QUrl genresUrl(QString genre = QString(), const QString & filter = QString()) { return url_api_v1 + QStringLiteral("genre.jsx?genre=%1&filter=%2").arg(genre, filter); }
+                inline QString genresUrl(QString genre = QString(), const QString & filter = QString()) { return url_api_v1 + QStringLiteral("genre.jsx?genre=%1&filter=%2").arg(genre, filter); }
 
 
-                inline QUrl topUrl(const QString & filter = QString(), QString genre = QStringLiteral("all")) { return url_api_v1 + QStringLiteral("top.jsx?genre=%1&filter=%2").arg(genre, filter); }
+                inline QString topUrl(const QString & filter = QString(), QString genre = QStringLiteral("all")) { return url_api_v1 + QStringLiteral("top.jsx?genre=%1&filter=%2").arg(genre, filter); }
 
-                inline QUrl searchUrl(const QString & predicate, const QString & sources = QStringLiteral("all")) { return url_api_v1 + QStringLiteral("music-search.jsx?text=%1&type=%2").arg(predicate, sources); }
+                inline QString searchUrl(const QString & predicate, const QString & sources = QStringLiteral("all")) { return url_api_v1 + QStringLiteral("music-search.jsx?text=%1&type=%2").arg(predicate, sources); }
 
                 // ncrnd=0.9069681936002026
                 // days: [{day, tracksToPlay: [], events: [{type = 'recomended-genre-radio', title: {}, genres: [id, artists:[{}], albums: []]}, {type='genre-top, tracks: [], genre, title: {type, text}'}]}]
-                inline QUrl feedsUrl() { return url_api_v1 + QStringLiteral("feed.jsx?nocheck="); }
+                inline QString feedsUrl() { return url_api_v1 + QStringLiteral("feed.jsx?nocheck="); }
 
                 // title, kind, trackCount, tracks, owner: {uid, login, name, verified}, cover: {type, dir, version, uri}, trackIds, modified
-                inline QUrl playlistUrl(const QString & owner) { return url_api_v1 + QStringLiteral("playlist.jsx?owner=%1&kinds=1267&light=true").arg(owner); }
+                inline QString playlistUrl(const QString & owner) { return url_api_v1 + QStringLiteral("playlist.jsx?owner=%1&kinds=1267&light=true").arg(owner); }
 
-                QJsonArray popular(QString & genre) {
-                    qDebug() << "__________________________________";
-                    return sQuery(topUrl(tkn_tracks, genre)).value(tkn_tracks).toArray();
+                QJsonArray popular(const SearchLimit & limits) {
+                    return sRequest(
+                        topUrl(tkn_tracks, limits.genre),
+                        call_type_json
+                    ).value(tkn_tracks).toArray();
+
+//                    return sQuery(topUrl(tkn_tracks, limits.genre)).value(tkn_tracks).toArray();
                 }
 
-                QJsonArray search_postprocess(QString & predicate, QString & genre, const SearchLimit & limitations) {
-                    if (predicate.isEmpty() || limitations.by_popularity()) {
-                        return popular(genre);
+                QJsonArray search_proc(const SearchLimit & limits) {
+                    if (limits.predicate.isEmpty() || limits.by_popularity()) {
+                        return popular(limits);
                     } else {
-                        if (limitations.by_artists())
-                            return sQuery(searchUrl(predicate, tkn_artists)).value(tkn_artists).toObject().value(QStringLiteral("items")).toArray();
-                        else
-                            return sQuery(searchUrl(predicate, tkn_tracks)).value(tkn_tracks).toObject().value(QStringLiteral("items")).toArray();
+                        QString tkn = limits.by_artists() ? tkn_artists : tkn_tracks;
+
+                        return sRequest(
+                            searchUrl(limits.predicate, tkn),
+                            call_type_json
+                        ).value(tkn).toObject().value(QStringLiteral("items")).toArray();
+
+//                        if (limits.by_artists())
+//                            return sQuery(searchUrl(limits.predicate, tkn_artists)).value(tkn_artists).toObject().value(tkn).toArray();
+//                        else
+//                            return sQuery(searchUrl(limits.predicate, tkn_tracks)).value(tkn_tracks).toObject().value(tkn).toArray();
                     }
                 }
             public:
