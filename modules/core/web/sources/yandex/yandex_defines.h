1477fe305b8ac586e9f98009603a8784c6d30e09
JB <jeyboy1985@gmail.com>
2016-09-22 10:34:09 +0300

    temp

--- modules/core/web/sources/yandex/yandex_defines.h
+++ modules/core/web/sources/yandex/yandex_defines.h
@@ -8,11 +8,12 @@
 #define YANDEX_SEARCH_LIMIT 200
 #define YANDEX_IDS_PER_REQUEST 50
 
-#define YANDEX_ITEM_UID(json) JSON_CSTR(json, tkn_id) % ':' % \
-        JSON_CSTR( \
-            (JSON_HAS_KEY(json, tkn_album) ? JSON_OBJ(json, tkn_album) : JSON_ARR(json, tkn_albums).first().toObject()), \
-            tkn_id \
-        )
+#define YANDEX_ITEM_ALBUM(json) JSON_CSTR( \
+        (JSON_HAS_KEY(json, tkn_album) ? JSON_OBJ(json, tkn_album) : JSON_ARR(json, tkn_albums).first().toObject()), \
+        tkn_id \
+    )
+
+#define YANDEX_ITEM_UID(json) JSON_CSTR(json, tkn_id) % ':' % YANDEX_ITEM_ALBUM(json)
 
 namespace Core {
     namespace Web {
--- modules/core/web/sources/yandex/yandex_misc.h
+++ modules/core/web/sources/yandex/yandex_misc.h
@@ -22,6 +22,14 @@ namespace Core {
 
                     Headers headers() { return {{ LSTR("X-Retpath-Y"), encodeStr(baseUrlStr(qst_site_base))}}; }
 
+                    // curl 'https://music.yandex.ua/api/v2.1/handlers/auth?external-domain=music.yandex.ua&overembed=no&__t=1474529009008' -H 'Accept: application/json; q=1.0, text/*; q=0.8, */*; q=0.1' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.5' -H 'Cache-Control: no-cache' -H 'Connection: keep-alive' -H 'Cookie: yandexuid=2190563221452621883; L=WnEJRFAPV0xrQVpRcXN0SEdPV0t6bmtBXl0rEDg8e3p/cg==.1474342480.12659.397393.538aa840261797a55723bf7670f887c4; yp=1464207350.ww.1#1472199353.szm.1%3A1920x1080%3A1920x969#1788794542.multib.1; _ym_uid=1473426341377070158; lastVisitedPage=%7B%22363617853%22%3A%22%2Ffeed%22%7D; yabs-vdrf=TR-DN203_mrS1pDnNIW57Yqy0pDnNEmOrC0y1pDnNWG5dJ5C1pDnNDm3LUrK1WDnN5GIw4Am0VjnNDm1Aa5G1hTbN201Z8L819ifNtWF8wa009ifNt01j_nG1USbNFm2Hxje0BCXN202rAXW1ZiTNIW6T4nu0lyPNDm0H3bS16iPNRW3cBLS1TiLNDm1yArq1eiHNDm3V1oi1eiHNt02WOLW1nxvN402CAm400; spravka=dD0xNDczNjY2OTQwO2k9MTc4LjEzNy4xMTIuMjM7dT0xNDczNjY2OTQwOTQ1MjcyMDI1O2g9OTYzMWExMzk1OWQ1NDQ1MWRiNWMwZWFiYTZhNTNkNzU=; Session_id=noauth:1474430260; device_id="b79263ca059d9f7d5505a415b6cf5632af8419b20"; _ym_isad=1' -H 'DNT: 1' -H 'Host: music.yandex.ua' -H 'Pragma: no-cache' -H 'Referer: https://music.yandex.ua/genre/electronic' -H 'User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:48.0) Gecko/20100101 Firefox/48.0' -H 'X-Requested-With: XMLHttpRequest' -H 'X-Retpath-Y: https%3A%2F%2Fmusic.yandex.ua%2Fgenre%2Felectronic'
+                    QString sign() {
+                        QJsonObject response = Manager::prepare() -> jsonGet(
+                            baseUrlStr(qst_site_alt2, LSTR("auth"))
+                        );
+                        return JSON_CSTR(response, LSTR("csrf"));
+                    }
+
                     QString calcKey(const QString & path, const QString & s) {
                         QString str = path.mid(1) + s;
     //                    str.replace(QStringLiteral("\r\n"), QStringLiteral("\n"))
