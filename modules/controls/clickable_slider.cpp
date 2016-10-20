0eb45124ebc7efcfec5e5c57be37193a72dcb1ab
JB <jeyboy1985@gmail.com>
2016-10-20 22:54:01 +0300

    improve icons

--- modules/controls/clickable_slider.cpp
+++ modules/controls/clickable_slider.cpp
@@ -3,10 +3,16 @@
 
 using namespace Controls;
 
-ClickableSlider::ClickableSlider(int default_value, QWidget * parent, const QString & css_rule) : QSlider(parent), default_value(default_value), rule(css_rule) {
+ClickableSlider::ClickableSlider(int default_value, QWidget * parent, const QString & css_rule) : QSlider(parent), default_value(default_value) {
     setStyle(slider_style = new SliderStyle());
-    if (!rule.isEmpty())
-        connect(this, SIGNAL(valueChanged(int)), this, SLOT(valieChanged(int)));
+    if (!css_rule.isEmpty()) {
+        connect(this, SIGNAL(valueChanged(int)), this, SLOT(valueChanged(int)));
+        QStringList parts = css_rule.split('|', QString::SkipEmptyParts);
+
+        rule = parts.first();
+        low_level = parts[1].toInt();
+        up_level = parts.last().toInt();
+    }
 
     if (default_value != -1)
         setContextMenuPolicy(Qt::DefaultContextMenu);
@@ -21,9 +27,12 @@ void ClickableSlider::contextMenuEvent(QContextMenuEvent * event) {
     event -> accept();
 }
 
-void ClickableSlider::valieChanged(int newVal) {
-    int percent = (newVal / (float)maximum() * 100);
-    Settings::currentStyle -> applyProperty(this, rule.toUtf8(), percent > 75 ? "full" : percent > 25 ? "half" : "semi");
+void ClickableSlider::valueChanged(int new_val) {
+    Settings::currentStyle -> applyProperty(
+        this,
+        rule.toUtf8(),
+        new_val < low_level ? "semi" : new_val > up_level ? "full" : "half"
+    );
 }
 
 void ClickableSlider::toDefault() { setValue(default_value > maximum() ? maximum() : default_value < minimum() ? minimum() : default_value); }
--- modules/controls/clickable_slider.h
+++ modules/controls/clickable_slider.h
@@ -14,20 +14,25 @@ namespace Controls {
         SliderStyle * slider_style;
         QString rule;
 
-        int default_value;
+        int default_value, low_level, up_level;
     protected:
         void contextMenuEvent(QContextMenuEvent * event);
     public:
         ClickableSlider(int default_value = -1, QWidget * parent = 0, const QString & css_rule = QString());
         ~ClickableSlider() { delete slider_style; }
     public slots:
+        void setValue(int val) {
+            if (value() == val) // fix when def value eq to set value
+                valueChanged(val);
+            QSlider::setValue(val);
+        }
         void setValueSilently(int val) {
             blockSignals(true);
             setValue(val);
             blockSignals(false);
         }
     protected slots:
-        void valieChanged(int newVal);
+        void valueChanged(int newVal);
         void toDefault();
     };
 }
